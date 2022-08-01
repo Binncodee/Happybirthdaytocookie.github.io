ca6114a620cbb62976d308d16e2caa661c6ac642
Trần Hoàng Hải <92655443+Binncodee@users.noreply.github.com>
2022-08-01 07:46:56 +0700

    UPLOAD

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em Yêu của Anh ❤';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy Birthday 🎂🎂';
+const titleCard = 'Cookie🥨 ơiiii😁 ';
+const contentCard = 'Cảm ơn em! Người mang cho anh niềm vui trong cuộc sống và hứng khởi trong công việc. Sinh nhật em Anh chúc em luôn xinh tươi, yêu đời, học thiệt là giỏi. Anh sẽ luôn bên em❤ Yêu e!';
+
+const giftImage = 'Lyy.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
