10d07856fe67ca124666e309e8c5cc28583d320a
Trần Hoàng Hải <92655443+Binncodee@users.noreply.github.com>
2022-08-21 21:18:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Em Yêu của Anh ❤';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Happy Birthday 🎂🎂';
 const titleCard = 'Cookie🥨 ơiiii😁 ';
-const contentCard = 'Cảm ơn em! Người mang cho anh niềm vui trong cuộc sống và hứng khởi trong công việc. Sinh nhật em Anh chúc em luôn xinh tươi, yêu đời, học thiệt là giỏi. Anh sẽ luôn bên em❤ Yêu e!';
+const contentCard = 'Cảm ơn em! Người mang cho anh niềm vui trong cuộc sống và hứng khởi trong công việc. Sinh nhật của em Anh chúc em luôn xinh tươi, yêu đời, học thiệt là giỏi. Anh cảm ơn em vì tất cả, chúc em có một sinh nhật tuổi 17 thật đẹp nhé ❤️';
 
 const giftImage = 'Lyy.png';
 const base64 = '';
